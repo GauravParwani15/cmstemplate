8f707a2599118c9b97c633613552eb321f6acaa7
tarunchhabriya06 <66588897+tarunchhabriya06@users.noreply.github.com>
2020-06-12 14:12:25 +0530

    Update application_data.blade.php

--- resources/views/faculty/pages/application_data.blade.php
+++ resources/views/faculty/pages/application_data.blade.php
@@ -48,20 +48,30 @@
                     <td colspan="3">Resource:-{{$data[0]->resource->name}}</td>
                 </tr>
                 <tr>
-                    <td colspan="3">date: {{$data[0]->event_date}}</td>
+                    <td colspan="3">Date: {{$data[0]->event_date}}</td>
                     <td colspan="3">Time: {{$data[0]->start_time}}-{{$data[0]->end_time}}</td>
                 </tr>
                 <tr>
-                    <td colspan="3">for class: {{$data[0]->for_crowd}}</td>
+                    <td colspan="3">For class: {{$data[0]->for_crowd}}</td>
                     <td colspan="3">Expected guests/speaker/company name: {{$data[0]->guest}}</td>
                 </tr>
                 <tr>
                     <td colspan="3">Expected crowd: {{$data[0]->expected_crowd}}</td>
                     <td colspan="3">User Mail: {{$data[0]->user_email}}</td>
                 </tr>
+                @if($data[0]->status==0)
                 <tr class="text-center">
                     <td colspan="6"><a href="{{ url ('/staff/manage_application') }}"><button class="btn btn-primary">Back</button></a></td>
                 </tr>
+                @elseif($data[0]->status==2)
+                <tr class="text-center">
+                    <td colspan="6"><a href="{{ url ('/staff/rejected_application') }}"><button class="btn btn-primary">Back</button></a></td>
+                </tr>
+                @else
+                <tr class="text-center">
+                    <td colspan="6"><a href="{{ url ('/staff/approved_application') }}"><button class="btn btn-primary">Back</button></a></td>
+                </tr>
+                @endif
             </tbody>
         </table>
     </div>    
